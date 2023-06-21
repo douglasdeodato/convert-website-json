fb360a2b90f8b198d1e8be1bd8673e7456956c30
douglas <douglasdeodato@gmail.com>
2023-06-21 12:13:34 +0100

    commit

--- convert.py
+++ convert.py
@@ -2,17 +2,34 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# 1. Faça uma solicitação HTTP para obter o conteúdo HTML do site
-response = requests.get('http://exemplo.com')
+# Carrega a URL do arquivo JSON
+with open('config.json', 'r') as arquivo:
+    dados_json = json.load(arquivo)
 
-# 2. Analise o conteúdo HTML usando BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+# Obtém a URL do JSON carregado
+url = dados_json['site_url']
 
-# 3. Extraia as informações relevantes e armazene em uma estrutura de dados
-dados = {}
-dados['titulo'] = soup.title.string
-dados['paragrafos'] = [p.get_text() for p in soup.find_all('p')]
+# Faz uma solicitação HTTP para obter o conteúdo HTML do site
+response = requests.get(url)
 
-# 4. Salve a estrutura de dados em um arquivo JSON
-with open('dados.json', 'w') as arquivo:
-    json.dump(dados, arquivo)
+# Verifica se a solicitação foi bem-sucedida
+if response.status_code == 200:
+    # Analisa o conteúdo HTML usando BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Cria um dicionário vazio para armazenar os dados
+    dados = {}
+
+    # Extrai o título do site
+    dados['titulo'] = soup.title.string
+
+    # Extrai os parágrafos do site
+    dados['paragrafos'] = [p.get_text() for p in soup.find_all('p')]
+
+    # Salva os dados em um arquivo JSON
+    with open('dados.json', 'w') as arquivo_saida:
+        json.dump(dados, arquivo_saida)
+
+    print("Dados salvos em dados.json com sucesso!")
+else:
+    print("Ocorreu um erro ao fazer a solicitação HTTP. Código de status:", response.status_code)
