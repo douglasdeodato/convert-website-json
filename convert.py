44cfbd31d8aa30c41728a66e430dd4668dcf797d
douglas <douglasdeodato@gmail.com>
2023-06-21 12:43:27 +0100

    get h1 e h4

--- convert.py
+++ convert.py
@@ -26,6 +26,12 @@ if response.status_code == 200:
     # Extract the paragraphs from the website
     data['paragraphs'] = [p.get_text() for p in soup.find_all('p')]
 
+    # Extract the h1 and h4 headings from the website
+    data['headings'] = {
+        'h1': [h1.get_text() for h1 in soup.find_all('h1')],
+        'h4': [h4.get_text() for h4 in soup.find_all('h4')]
+    }
+
     # Save the data to a JSON file with formatting
     with open('data.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
