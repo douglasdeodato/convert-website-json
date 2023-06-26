478b9ba739cd589ef43a5c55604c70105733cb5d
douglas <douglasdeodato@gmail.com>
2023-06-26 14:35:53 +0100

    condition inserted

--- convert2.py
+++ convert2.py
@@ -7,11 +7,17 @@ import sys
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-# Create a list to store all the extracted href links
+# Load the stop condition from the config.json file
+with open('config.json', 'r') as file:
+    config_data = json.load(file)
+stop_condition = config_data['stop_condition']
+
+# Create a list to store all the extracted links
 extracted_links = []
 
 # Iterate over the paragraphs in the data
 checked_count = 0  # Variable to count the checked links
+stop_check = False  # Flag to control the link check
 
 for paragraph_data in data['paragraphs']:
     text = paragraph_data['text']
@@ -29,22 +35,39 @@ for paragraph_data in data['paragraphs']:
                     # Append the href values to the extracted_links list
                     extracted_links.extend(linked_hrefs)
                 checked_count += 1
-                print("Checking link:", href)
+                print("\nChecking link:", href)
                 print("Text:", text)  # Print the text value
-                print(f"{checked_count} link checked")  # Print the count message
+                print(f"{checked_count} link checked\n")  # Print the count message
                 sys.stdout.flush()  # Flush the output buffer to show the message immediately
+
+                # Check if the text value matches the stop condition
+                if text == stop_condition:
+                    stop_check = True
+                    break
+
+                # Append the "text" and "url converted" fields to the extracted_links list
+                extracted_links.append({
+                    "text": text,
+                    "url converted": href
+                })
+
     else:
-        print("No links found.")
+        print("\nNo links found.")
         print("Text:", text)  # Print the text value
         sys.stdout.flush()  # Flush the output buffer to show the message immediately
 
+    # Break the loop if the stop condition is met
+    if stop_check:
+        break
+
 # Create a dictionary to store the extracted links
 extracted_data = {
-    'links': extracted_links
+    'stop_condition': stop_condition,
+    'links': [None] + extracted_links  # Insert null as the first element
 }
 
 # Save the extracted data to a JSON file with formatting
 with open('data2.json', 'w') as outfile:
     json.dump(extracted_data, outfile, indent=4)
 
-print("Data saved to data2.json successfully!")
+print("\nData saved to data2.json successfully!")
